7fd30fd7811c2df632055e08f03d508fd4494085
yurashk <Yuriy.Gienko@fuib.com>
2022-06-10 19:26:00 +0300

    twent-commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const path=require('path')
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 8080
 const app=express()
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname,'dist/vag-cars-trecker')))
